c031efbb1de17e1c70160332b0fb3385c6f87c73
Oleksii Demedetskyi <oleksii.d@sbtech.com>
2021-02-17 18:50:01 +0200

    Initial Commit

---
+++ RecoilDemo/RecoilDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecoilDemoApp.swift
+//  RecoilDemo
+//
+//  Created by Oleksii Demedetskyi on 17.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct RecoilDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
