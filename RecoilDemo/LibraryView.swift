ef74070fca4e9722ea69f9328c141afeb4ee3028
Oleksii Demedetskyi <oleksii.d@sbtech.com>
2021-03-11 12:49:20 +0200

    Stage 0

--- RecoilDemo/LibraryView.swift
+++ RecoilDemo/LibraryView.swift
@@ -8,39 +8,8 @@
 import SwiftUI
 
 struct LibraryView: View {
-    let library = LibraryState()
-    let newBookState = NewBookState()
-    
     var body: some View {
-        TextField("Search", text: library.$query)
-        
-        Picker("Section", selection: library.$section) {
-            Text("All").tag(LibraryState.Section.all)
-            Text("Favorites").tag(LibraryState.Section.favorites)
-        }
-        .pickerStyle(SegmentedPickerStyle())
-        
-        List(library.books) { book in
-            BookView(book: book)
-        }
-        
-        Button("Add") { newBookState.addBook() }
-            .disabled(!newBookState.canAddBook)
-        
-        TextField("Name", text: newBookState.$name)
-        TextField("Author", text: newBookState.$author)
-    }
-}
-
-struct BookView: View {
-    let book: BookState
-    
-    var body: some View {
-        Image(systemName: "star.fill")
-            .foregroundColor(book.isFavorite ? .yellow : .gray)
-            .onTapGesture { book.isFavorite.toggle() }
-        
-        Text("\(book.name) by \(book.author)")
+        EmptyView()
     }
 }
 
